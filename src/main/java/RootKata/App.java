f7c3c8fc97365bca4705c7b65dac06c511808a81
Sherief Gharraph <sherief@gharraph.com>
2019-03-31 18:51:32 -0400

    modified git ignore

--- src/main/java/RootKata/App.java
+++ src/main/java/RootKata/App.java
@@ -5,7 +5,7 @@ package RootKata;
 
 public class App {
     public String getGreeting() {
-        return "Hello world.";
+        return "Hello Root Driver.";
     }
 
     public static void main(String[] args) {
